2841e8699944cd5bf7d37958f0955fdfa9976f7b
Administrator <Administrator@koitt03a-PC>
2019-09-27 15:17:19 +0900

    커밋만할껴

--- src/study01/test03/HowOldRU.java
+++ src/study01/test03/HowOldRU.java
@@ -10,5 +10,19 @@ public class HowOldRU {
 		String age = scan.nextLine();
 		System.out.println("난 " + age + "살 이야~");
 		int num = Integer.parseInt(age);
+		//받은 나이가 10살 이하면 어린이라고 출력
+		//20살 이하면 청소년이라고 출력
+		//40살 이하면 청년이라고 출력
+		//41살 이상이면 꼰대라고 출력
+		
+		if(num<=10) {
+			System.out.println("어린이");
+		}else if(num<=20) {
+			System.out.println("청소년");
+		}else if(num<=40) {
+			System.out.println("청년");
+		}else if(num>=41) {
+			System.out.println("꼰대");
+		}
 	}
 }
